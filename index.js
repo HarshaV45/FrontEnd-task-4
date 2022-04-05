dc59a1ceee107b6a4cc28b55af04ba452554b57f
Harsha <harshavadlavalli4@gmail.com>
2022-04-05 08:53:07 +0530

    imlemented html and css part

---
+++ index.js
@@ -0,0 +1,42 @@
+var data = [
+    {
+        "id":"1",
+        "first_name":"Anand",
+        "last_name":"sai",
+        "preferred_name":"Anand sai",
+        "job_title":"SharePoint Practice Head",
+        "department":"IT Department",
+        "email":"anand.m@gmail.com",
+        "office":"Seattle",
+        "phone_number":"9246289627",
+        "skype_id":"9472874852818",
+        "profile_pic":"./Assets/profile1.jpg"
+    },
+    {
+        "id":"2",
+        "first_name":"kumar",
+        "last_name":"krishna",
+        "preferred_name":"Kumar Krishna",
+        "job_title":"Operations Manager",
+        "department":"IT Department",
+        "email":"kumar.k@gmail.com",
+        "office":"Seattle",
+        "phone_number":"7264128312",
+        "skype_id":"8235135895281",
+        "profile_pic":"./Assets/profile2.jpg"
+    },
+    {
+        "id":"3",
+        "first_name":"surya",
+        "last_name":"williams",
+        "preferred_name":"surya williams",
+        "job_title":"Product Manager",
+        "department":"IT Department",
+        "email":"suryawilliams@gmail.com",
+        "office":"Seattle",
+        "phone_number":"7527637272",
+        "skype_id":"7254161763716",
+        "profile_pic":"./Assets/profile3.jpg"
+    }
+    
+]
